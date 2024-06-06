d86093a1da025e7daac65bf6b7c5af9eddd7a61a
Hasbem <109442038+Hasbem@users.noreply.github.com>
2024-06-06 12:14:36 +0200

    ajout du background

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,13 @@
+import "./style/Homepage.css";
+
+function Homepage() {
+  return (
+    <>
+      <div>
+        <div className="background"></div>
+      </div>
+    </>
+  );
+}
+
+export default Homepage;
