491a8e97730d188d030e64b60b02320b1c6669da
SMAIL ALAHYANE <sm.alahyane@gmail.com>
2024-06-06 22:35:53 +0200

    l'ajout des images

---
+++ src/components/ActivityCard.jsx
@@ -0,0 +1,30 @@
+import { useNavigate } from "react-router-dom";
+import activities from "../activities";
+import "../style/ActivityCard.css";
+
+export default function ActivityCard() {
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="activities">
+        <h1 className="titleHead"> Activités :</h1>
+        <h2 className="paraph">
+          Nous vous présentons nos activités disponibles :
+        </h2>
+      </div>
+      <div className="card_container">
+        {activities.map((element) => (
+          <div
+            key={element.id}
+            className="picture"
+            onClick={() => navigate(`/details/${element.id}`)}
+            style={{ background: `url(${element.img}) center/cover` }}
+            role="presentation"
+          >
+            <h3 className="descriptionAct">{element.title}</h3>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
